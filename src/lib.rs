004e5f7e47942dfbede23b9ade3bb7ae13d626bf
cazacuradu <cazacuraduic@gmail.com>
2022-09-17 14:43:10 +0300

    Boilerplate project

---
+++ src/lib.rs
@@ -0,0 +1,69 @@
+#![recursion_limit = "512"]
+
+// mod components;
+// mod services;
+
+use wasm_bindgen::prelude::*;
+use yew::functional::*;
+use yew::prelude::*;
+use yew_router::prelude::*;
+
+use std::cell::RefCell;
+use std::rc::Rc;
+
+pub type Images = Rc<ImagesInner>;
+
+#[derive(PartialEq, Debug)]
+pub struct ImagesInner {
+    pub images: RefCell<String>,
+}
+#[derive(Debug, Clone, Copy, PartialEq, Routable)]
+pub enum Route {
+    #[at("/")]
+    Main,
+    #[not_found]
+    #[at("/404")]
+    NotFound,
+}
+
+fn switch(selected_route: &Route) -> Html {
+    match selected_route {
+        Route::Main => {
+            html! {<button class="text-black bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">{"Default"}</button>}
+        }
+        Route::NotFound => html! {<h1>{"404 baby"}</h1>},
+    }
+}
+
+#[function_component(Main)]
+fn main() -> Html {
+    let ctx = use_state(|| {
+        Rc::new(ImagesInner {
+            images: RefCell::new("initial".into()),
+        })
+    });
+
+    html! {
+            <ContextProvider<Images> context={(*ctx).clone()}>
+        <BrowserRouter>
+            <div class="w-screen h-screen">
+                <Switch<Route> render={Switch::render(switch)} />
+            </div>
+        </BrowserRouter>
+                </ContextProvider<Images>>
+    }
+}
+
+#[wasm_bindgen]
+pub fn run_app() -> Result<(), JsValue> {
+    wasm_logger::init(wasm_logger::Config::default());
+    yew::start_app::<Main>();
+    Ok(())
+}
+// When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
+// allocator.
+//
+// If you don't want to use `wee_alloc`, you can safely delete this.
+// #[cfg(feature = "wee_alloc")]
+// #[global_allocator]
+// static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
+
+const distPath = path.resolve(__dirname, "dist");
+var mode = process.env.NODE_ENV || "development";
+module.exports = {
+  devtool: mode === "development" ? "inline-source-map" : false,
+  mode: mode,
+
+  devServer: {
+    port: 8000,
+  },
+  entry: "./bootstrap.js",
+  output: {
+    path: distPath,
+    filename: "index.js",
+    webassemblyModuleFilename: "pkg.wasm",
+    clean: true,
+  },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [{ from: "./static", to: distPath }],
+    }),
+    new WasmPackPlugin({
+      crateDirectory: ".",
+      extraArgs: "-- --features wee_alloc",
+      outName: "index",
+      forceMode: mode,
+      forceWatch: true,
+      watchOptions: {
+        aggregateTimeout: 500,
+        poll: 200,
+        //  is not necessary as long as you remove pkg/* before building your wasm files
+      },
+      watchDirectories: [
+        // make distinct from outDir
+        path.resolve(__dirname, "./src"), // point to rs src
+      ],
+    }),
+  ],
+  experiments: {
+    syncWebAssembly: true,
+  },
+};
